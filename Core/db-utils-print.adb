e8c422ccb832d780debc3ffc8298d980714cb95c
Christoph Schwering <schwering@gmail.com>
2009-12-24 13:39:58 +0100

    Added forgotten Print procedure.

---
+++ Core/db-utils-print.adb
@@ -0,0 +1,14 @@
+-- Abstract:
+--
+-- see spec
+--
+-- Copyright 2008, 2009 Christoph Schwering
+
+procedure DB.Utils.Print (S : String)
+is
+   procedure Internal (S : String);
+   pragma Import (C, Internal, "printf");
+begin
+   Internal(S & ASCII.LF & ASCII.NUL);
+end DB.Utils.Print;
+
