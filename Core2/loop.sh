25f69f25548d4afa51314feb1033986e351fa622
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2010-05-15 14:41:25 +0200

    Improved ./loop; rm -f .tmp/btree must be argument now.

--- Core2/loop.sh
+++ Core2/loop.sh
@@ -5,8 +5,7 @@ outf=".out"
 while [ ! -n "$out" ]
 do
         echo "Executing $cmd"
-        rm -f .tmp/btree
-        $cmd 2>&1 >"$outf"
+        sh -c "$cmd" 2>&1 >"$outf"
         retval=$?
         echo $retval
         cat "$outf"
