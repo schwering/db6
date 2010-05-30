80afdf2f3ccfeb8424621e9159d7823e45db08e7
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2010-05-30 17:54:41 +0200

    Dcumentation.

--- Core2/db.ads
+++ Core2/db.ads
@@ -2,13 +2,16 @@
 --
 -- The root package. Important children are:
 --
--- DB.Gen_BTrees -- The core data structure for key/value pairs.
--- DB.Gen_Heaps  -- Data structure for large values.
--- DB.IO.Blocks  -- Block IO implementations.
--- DB.Types      -- Key and value types for the BTree, including strings.
+-- DB.Maps       -- A general object-oriented maps API which essentially
+--                  forwards to a Gen_BTrees instance.
+-- DB.Gen_BTrees -- A generic Yao & Lehman B-tree for high concurrency.
+--                  The core data structure for key/value pairs.
+-- DB.Blocks     -- Block IO implementations.
+-- DB.Types      -- Key and value types for the Gen_BTrees instance used in
+--                  DB.Maps. Would be child package of DB.Maps if the compiler
+--                  would accept it (he complains about circular dependencies).
 -- DB.Locks      -- Locking mechanisms.
--- DB.Utils      -- Miscellaneous things.
--- DB.Tables     -- Table wrapper.
+-- DB.Utils      -- Miscellaneous things like a binary search, queues, stacks.
 --
 -- Copyright 2008, 2009, 2010 Christoph Schwering
 
@@ -28,20 +31,8 @@ package DB is
    -- nodes.
    -- Raised in DB.Gen_BTrees and DB.Gen_Blob_Trees.
 
-   Heap_Error : exception;
-   -- This exception is only raised when there are extremely serious
-   -- errors in the heap such as dangling references to child or neighbor
-   -- nodes.
-   -- Raised in DB.Gen_Heaps.
-
    Timer_Error : exception;
    -- Indicates wrong usage of timers.
-
-   Hash_Table_Error : exception;
-   -- An error in the fixed hash table.
-
-   List_Error : exception;
-   -- An error in the linked list.
 
    Map_Reduce_Error : exception;
    -- An error occurred during map/reduce.
