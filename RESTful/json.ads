3dc8b7c4431e25559294868e7f6d39c16b46c684
Christoph Schwering <schwering@gmail.com>
2010-12-20 04:33:49 +0100

    First JSON serialization attempt.
    However, this attempt is unnecessarily complicated, I think.
    It would suffice to directly translate the Value_Type objects into
    JSON and store this in a big buffer.

--- RESTful/json.ads
+++ RESTful/json.ads
@@ -9,6 +9,9 @@ with Ada.Streams;
 with AWS.Resources.Streams;
 
 private with DB.DSA.Utils.Gen_Queues;
+with DB.Maps.Values.Long_Floats;
+with DB.Maps.Values.Long_Integers;
+with DB.Maps.Values.Strings;
 
 package JSON is
    pragma Elaborate_Body;
@@ -17,6 +20,47 @@ package JSON is
    package ARS renames AWS.Resources.Streams;
 
    type Stream_Type is new ARS.Stream_Type with private;
+
+   ----------
+   -- Operations to manipulate the content of the stream.
+
+   subtype Float_Value_Type is DB.Maps.Values.Long_Floats.Value_Type'Class;
+   subtype Integer_Value_Type is DB.Maps.Values.Long_Integers.Value_Type'Class;
+   subtype String_Value_Type is DB.Maps.Values.Strings.Value_Type'Class;
+
+   procedure Put_Float
+     (Resource : in out Stream_Type;
+      Key      : in     String;
+      Float    : in     Float_Value_Type);
+
+   procedure Put_Integer
+     (Resource : in out Stream_Type;
+      Key      : in     String;
+      Integer  : in     Integer_Value_Type);
+
+   procedure Put_String
+     (Resource : in out Stream_Type;
+      Key      : in     String;
+      Str      : in     String_Value_Type);
+
+   procedure Put_Null
+     (Resource : in out Stream_Type;
+      Key      : in     String);
+
+   procedure Put_Object
+     (Resource : in out Stream_Type;
+      Key      : in     String);
+
+   procedure Put_Array
+     (Resource : in out Stream_Type;
+      Key      : in     String);
+
+   procedure End_Object (Resource : in out Stream_Type);
+
+   procedure End_Array (Resource : in out Stream_Type);
+
+   ----------
+   -- Operations to read the stream.
 
    procedure Read
      (Resource : in out Stream_Type;
@@ -33,34 +77,31 @@ package JSON is
      (Resource : in out Stream_Type;
       To       : in     AS.Stream_Element_Offset) is null;
 
-
 private
-   type Marker_Type is (JSON_Array_Begin,  JSON_Array_End,
-                        JSON_Object_Begin, JSON_Object_End);
+   type Marker_Type is (JSON_Array_End, JSON_Object_End);
 
-   type JSON_Type is (JSON_Number, JSON_String, JSON_Boolean, JSON_Null);
+   type JSON_Type is (JSON_Number, JSON_String, JSON_Boolean, JSON_Null,
+                      JSON_Object, JSON_Array);
 
    type String_Ref_Type is access String;
 
-   type Number_Type (Real : Boolean := False) is
+   type Number_Type (Is_Real : Boolean := False) is
       record
-         case Real is
-            when False => I : Long_Integer;
-            when True  => R : Long_Float;
+         case Is_Real is
+            when False => Int  : Long_Integer;
+            when True  => Real : Long_Float;
          end case;
       end record;
 
    type Value_Type (Kind : JSON_Type := JSON_Null) is
       record
          case Kind is
-            when JSON_Boolean =>
-               B : Boolean;
-            when JSON_Number =>
-               N : Number_Type;
-            when JSON_String =>
-               S : String_Ref_Type;
-            when JSON_Null =>
-               null;
+            when JSON_Boolean => B : Boolean;
+            when JSON_Number  => N : Number_Type;
+            when JSON_String  => S : String_Ref_Type;
+            when JSON_Null    => null;
+            when JSON_Object  => null;
+            when JSON_Array   => null;
          end case;
       end record;
 
@@ -86,8 +127,10 @@ private
 
    type Stream_Type is new ARS.Stream_Type with
       record
-         Queue : Queues.Queue_Type;
-         Final : Boolean := False;
+         Queue  : Queues.Queue_Type;
+         Buffer : String_Ref_Type := null;
+         Init   : Boolean := False;
+         Final  : Boolean := False;
       end record;
 
 end JSON;
