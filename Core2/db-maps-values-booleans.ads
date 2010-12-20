8962e1a9e49dac7bcbd8658b1a9f56314149803a
Christoph Schwering <schwering@gmail.com>
2010-12-20 17:53:19 +0100

    Added Boolean instance of integer Value_Type implementation.

---
+++ Core2/db-maps-values-booleans.ads
@@ -0,0 +1,10 @@
+-- Abstract:
+--
+-- Normal boolean value implementation.
+--
+-- Copyright 2008, 2009, 2010 Christoph Schwering
+
+with DB.Maps.Values.Gen_Integers;
+
+package DB.Maps.Values.Booleans is new Gen_Integers (Boolean);
+
