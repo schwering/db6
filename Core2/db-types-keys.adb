15097931360844ed93b944bdee82f161fc72a169
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2011-01-22 01:40:01 +0100

    Keys: column is now written
    This will probably destroy the *.sh tests!!!
    Because the keys get too large.
    Generation of pseudo-random data must be adapted.

--- Core2/db-types-keys.adb
+++ Core2/db-types-keys.adb
@@ -23,12 +23,12 @@ package body DB.Types.Keys is
       if Left.Row < Right.Row then
          return True;
       elsif Left.Row = Right.Row then
-         --if Left.Column < Right.Column then
-            --return True;
-         --elsif Left.Column = Right.Column then
+         if Left.Column < Right.Column then
+            return True;
+         elsif Left.Column = Right.Column then
             return Right.Time < Left.Time;
             -- intentionally L > R (most recent time is smallest)
-         --end if;
+         end if;
       end if;
       return False;
    end "<";
@@ -39,12 +39,12 @@ package body DB.Types.Keys is
       if Left.Row < Right.Row then
          return True;
       elsif Left.Row = Right.Row then
-         --if Left.Column < Right.Column then
-            --return True;
-         --elsif Left.Column = Right.Column then
+         if Left.Column < Right.Column then
+            return True;
+         elsif Left.Column = Right.Column then
             return not (Left.Time < Right.Time);
             -- intentionally !(L < R) <=> L >= R (most recent time is smallest)
-         --end if;
+         end if;
       end if;
       return False;
    end "<=";
@@ -58,9 +58,9 @@ package body DB.Types.Keys is
       use type Times.Number_Type;
       pragma Warnings (On);
    begin
-      return Left.Row = Right.Row
-         --and Left.Column = Right.Column
-         and Left.Time = Right.Time;
+      return Left.Row = Right.Row and then
+             Left.Column = Right.Column and then
+             Left.Time = Right.Time;
    end "=";
 
 
@@ -73,10 +73,10 @@ package body DB.Types.Keys is
       if C /= Utils.Equal then
          return C;
       end if;
-      --C := Columns.Compare (Left.Column, Right.Column);
-      --if C /= Utils.Equal then
-         --return C;
-      --end if;
+      C := Columns.Compare (Left.Column, Right.Column);
+      if C /= Utils.Equal then
+         return C;
+      end if;
       C := Times.Compare (Left.Time, Right.Time);
       case C is
          when Utils.Less    => return Utils.Greater;
@@ -117,16 +117,16 @@ package body DB.Types.Keys is
             Column_Delim : constant Columns.String_Type :=
                Columns.Short_Delimiter (Left.Column, Right.Column);
          begin
-            --if Left.Column < Column_Delim then
-            --   return Key_Type'(Row    => Row_Delim,
-            --                    Column => Column_Delim,
-            --                    Time   => Times.Number_Type'Last);
-            --else
+            if Left.Column < Column_Delim then
+               return Key_Type'(Row    => Row_Delim,
+                                Column => Column_Delim,
+                                Time   => Times.Number_Type'Last);
+            else
                Key :=  Key_Type'(Row    => Row_Delim,
                                  Column => Column_Delim,
                                  Time   => Times.Short_Delimiter (Left.Time,
                                                                   Right.Time));
-            --end if;
+            end if;
          end;
       end if;
       pragma Assert (Left /= Right or else (Left = Key and Key = Right));
@@ -156,7 +156,7 @@ package body DB.Types.Keys is
       use type Blocks.Size_Type;
    begin
       return Row_Serialization.Size_Bound (Key.Row) +
-           --Column_Serialization.Size_Bound (Key.Column) +
+             Column_Serialization.Size_Bound (Key.Column) +
              Times.Size_Bound (Key.Time);
    end Size_Bound;
 
@@ -168,8 +168,8 @@ package body DB.Types.Keys is
       Key     : in     Key_Type) is
    begin
       Row_Serialization.Write (Context.Row_Context, Block, Cursor, Key.Row);
-      --Column_Serialization.Write (Context.Column_Context, Block, Cursor,
-                                  --Key.Column);
+      Column_Serialization.Write (Context.Column_Context, Block, Cursor,
+                                  Key.Column);
       Times.Write (Context.Time_Context, Block, Cursor, Key.Time);
    end Write;
 
@@ -182,8 +182,8 @@ package body DB.Types.Keys is
    begin
       Row_Serialization.Read (Context.Row_Context, Block, Cursor, Key.Row);
       Key.Column := Columns.Empty_String;
-      --Column_Serialization.Read (Context.Column_Context, Block, Cursor,
-                                   --Key.Column);
+      Column_Serialization.Read (Context.Column_Context, Block, Cursor,
+                                 Key.Column);
       Times.Read (Context.Time_Context, Block, Cursor, Key.Time);
    end Read;
 
@@ -195,10 +195,10 @@ package body DB.Types.Keys is
    is
       Key : Key_Type;
    begin
-      Read (Context, Block, Cursor, Key);
-      --Row_Serialization.Skip (Context.Row_Context, Block, Cursor);
-      ----Column_Serialization.Skip (Context.Column_Context, Block, Cursor);
-      --Times.Skip (Context.Time_Context, Block, Cursor);
+      --Read (Context, Block, Cursor, Key);
+      Row_Serialization.Skip (Context.Row_Context, Block, Cursor);
+      Column_Serialization.Skip (Context.Column_Context, Block, Cursor);
+      Times.Skip (Context.Time_Context, Block, Cursor);
    end Skip;
 
 
